436772a5c4257c735c31a91444238057e552f187
Rodrigo de Sousa Santos <santos_ro@hotmail.com>
2020-08-30 14:03:32 -0300

    feat: create constants file and define initial route

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:stalkerapp/constants.dart';
+
+class SearchScreen extends StatelessWidget {
+  static const String id = '/search';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: kBackgroundColor,
+    );
+  }
+}
