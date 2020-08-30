9b577169be9ab27ba31cc8a39394fc1f0893dede
Rodrigo de Sousa Santos <santos_ro@hotmail.com>
2020-08-30 15:08:16 -0300

    feat: add http for network connection and consume name of the first starred app of a username

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:stalkerapp/constants.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class SearchScreen extends StatelessWidget {
   static const String id = '/search';
 
   @override
   Widget build(BuildContext context) {
+    final String username = 'digocse';
+    final String url = 'https://api.github.com/users/${username}/starred';
+
     return Container(
       color: kBackgroundColor,
+      child: FlatButton(
+        child: Text(
+          'Buscar',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 18.0,
+          ),
+        ),
+        onPressed: () async {
+          var response = await http.get(url);
+          if (response.statusCode == 200) {
+            var jsonResponse = convert.jsonDecode(response.body);
+            print(jsonResponse[0]["name"]);
+          } else {
+            print('Request failed with status: ${response.statusCode}');
+          }
+        },
+      ),
     );
   }
 }
